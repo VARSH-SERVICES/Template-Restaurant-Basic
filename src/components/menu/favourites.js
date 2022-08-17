368b71cc657cea884a8dccb7b92cd47d7cb5e2d3
Harsh <harsk7134@gmail.com>
2022-08-17 19:36:22 +0530

    first commit

---
+++ src/components/menu/favourites.js
@@ -0,0 +1,24 @@
+import barbecue from "../../images/barbecue_pizza.png";
+import pasta from "../../images/pasta.png";
+import burger from "../../images/burger.png";
+
+export const favorites = [
+  {
+    id: 1,
+    name: "barbecue pizza",
+    price: 450,
+    url: barbecue,
+  },
+  {
+    id: 2,
+    name: "cream cheese pasta",
+    price: 250,
+    url: pasta,
+  },
+  {
+    id: 3,
+    name: "double deluxe burger",
+    price: 100,
+    url: burger,
+  },
+];
---
+++ src/components/menu/meals.js
@@ -0,0 +1,253 @@
+//array of objects with meal data
+export const meals = [
+  {
+    id: 1,
+    name: "Tomato Shorba",
+    description:
+      "A twist on our dish with spicy dipping sauce.",
+    price: 80,
+    course: "Starters",
+  },
+  {
+    id: 2,
+    name: "French Fried",
+    description:
+      "Deep fried potato cheese sticks with spicy sauce and garlic salt.",
+    price: 130,
+    course: "Starters",
+  },
+  {
+    id: 3,
+    name: "Cream Soup",
+    description:
+      "With Brocolli/ mushroom / vegetable.",
+    price: 100,
+    course: "Starters",
+  },
+  {
+    id: 4,
+    name: "Salad",
+    description: "Lettuce, tomatoes, sweet pepper & shredded carrot..",
+    price: 80,
+    course: "Starters",
+  },
+  {
+    id: 5,
+    name: "Soup of the Day",
+    description: "Made fresh daily. Ask your server for today's choice.",
+    price: 120,
+    course: "Starters",
+  },
+  {
+    id: 6,
+    name: "Andhra Chilli Chicken",
+    description:
+      "Tender cubes of boneless chicken breast with spicy green chilli masala",
+    price: 180,
+    course: "Starters",
+  },
+
+  {
+    id: 7,
+    name: "Chicken Wrap ",
+    description:
+      "Our famous chicken with spicy sauce, cheddar cheese, lettuce, tomato & fried plantain.",
+    price: 165,
+    course: "Starters",
+  },
+  {
+    id: 8,
+    name: "Mutton chukka",
+    description:
+      "Deep fried and perfectly seasoned, served with a side of vegetables .",
+    price: 252,
+    course: "Starters",
+  },
+  {
+    id: 9,
+    name: "Indo-Chinese Chicken Lollypops",
+    description: "Crisp fried lollypop tossed with chilli, garlic & soya sauce. Ask your server for today's choice.",
+    price: 200,
+    course: "Starters",
+  },
+  {
+    id: 10,
+    name: "Fish Fry",
+    description:
+      "Choice of Nethili. Served with a side of plantains",
+    price: 163,
+    course: "Starters",
+  },
+
+  {
+    id: 11,
+    name: "Chicken Alfredo",
+    description:
+      "Made with heavy cream, garlic, parmesan cheese, fettuccine noodles, and tender chicken breasts for a classic Italian meal",
+    price: 248,
+    course: "Main Dishes",
+  },
+  {
+    id: 12,
+    name: "Parda Jhinga Kebab",
+    description:
+      "King prawns marinated with lemon & mustard, char grilled, wrapped in roomali.",
+    price: 320,
+    course: "Main Dishes",
+  },
+  {
+    id: 13,
+    name: "Coconut Curry Shrimp",
+    description:
+      "Fresh juicy shrimp fried with coconot flakes then slow simmered with local seasonings, in a spicy West Indian style curry sauce.",
+    price: 163,
+    course: "Main Dishes",
+  },
+  {
+    id: 14,
+    name: "Herb Grilled Salmon",
+    description:
+      "A simple soy sauce and brown sugar marinade, with hints of lemon and garlic, are the perfect salty-sweet complement to rich salmon fillets.",
+    price: 215,
+    course: "Main Dishes",
+  },
+  {
+    id: 15,
+    name: "Cheese ravioli",
+    description:
+      "Tasty ravioli recipe made with ricotta, cream cheese, mozzarella cheese, and provolone cheese. Served with marinara and pesto cream sauce.",
+    price: 170,
+    course: "Main Dishes",
+  },
+  {
+    id: 16,
+    name: "Jerk Chicken Dish",
+    description:
+      "A famous Jamaican dish. A spicy grilled-meat dish cooked to perfection with a blend of local seasonings. Paired with Jamaican rice and peas and fried ripe plantains.",
+    price: 195,
+    course: "Main Dishes",
+  },
+  {
+    id: 17,
+    name: "Curried Goat",
+    description:
+      "A traditional Jamaican made with mutton that is infused with flavours of curry powder, chopped onion, escallion, garlic, ginger, thyme and scotch bonnet pepper.",
+    price: 236,
+    course: "Main Dishes",
+  },
+  {
+    id: 18,
+    name: "Machi tikka",
+    description:
+      "The perfect roased BASSA FISH FILLET stew. Delicious and tender oxtail and butter beans that is cooked to perfection.",
+    price: 170,
+    course: "Main Dishes",
+  },
+  {
+    id: 19,
+    name: "Grilled Chicken",
+    description:
+      "Tender Caribbean chicken seasoned with jerk spices & perfectly grilled. Served with tangy papaya salsa and crackers.",
+    price: 310,
+    course: "Main Dishes",
+  },
+  {
+    id: 20,
+    name: "Steamed Fish",
+    description:
+      "Lightly seasoned red snapper steamed with an assortment of bell peppers and onions with a side serving of bammy.",
+    price: 125,
+    course: "Main Dishes",
+  },
+  {
+    id: 21,
+    name: "Tiramisu",
+    description:
+      "A classic Italian no-bake dessert made with layers of coffee-soaked ladyfingers and incredible mascarpone cream.",
+    price: 140,
+    course: "Desserts",
+  },
+  {
+    id: 22,
+    name: "Black Tie Mousse Cake",
+    description:
+      "Rich layers of chocolate cake, dark chocolate cheesecake and creamy custard mousse.",
+    price: 115,
+    course: "Desserts",
+  },
+  {
+    id: 23,
+    name: "Ice Cream of the day",
+    description: "Ask your server for today's choice.",
+    price: 850,
+    course: "Desserts",
+  },
+  {
+    id: 24,
+    name: "Carrot Cake",
+    description:
+      "Simple, sweet, moist, flavorful, and topped with cream cheese frosting!",
+    price: 120,
+    course: "Desserts",
+  },
+  {
+    id: 25,
+    name: "Doughnuts",
+    description:
+      "Made from soft pillowy dough, these sweet treats are then deep-fried and coated in sugar, before filling.",
+    price: 105,
+    course: "Desserts",
+  },
+  {
+    id: 26,
+    name: "Cheescake",
+    description:
+      "A rich, creamy texture and cherry topping for a showstopping dessert.",
+    price: 176,
+    course: "Desserts",
+  },
+  {
+    id: 27,
+    name: "Beers",
+    description: "Quench your thirst with any of our well chilled beers!",
+    price: 200,
+    course: "Drinks",
+  },
+  {
+    id: 28,
+    name: "Water",
+    description: "Quench your thirst with a glass of chilled water!",
+    price: 20,
+    course: "Drinks",
+  },
+  {
+    id: 29,
+    name: "Sodas",
+    description: "Quench your thirst with any of our chilled sodas!",
+    price: 20,
+    course: "Drinks",
+  },
+  {
+    id: 30,
+    name: "Piña Colada",
+    description:
+      "A tropical blend of rich coconut cream, white rum and tangy pineapple – serves with an umbrella for kitsch appeal.",
+    price: 50,
+    course: "Drinks",
+  },
+  {
+    id: 31,
+    name: "Strawberry Daiquiri",
+    description: "A smooth blend of simple syrup, rum and lime juice.",
+    price: 60,
+    course: "Drinks",
+  },
+  {
+    id: 32,
+    name: "Fruit Punch",
+    description:
+      "Non-alcoholic drink with fresh fruit, mint, juice and lemonade, it's wonderfully refreshing.",
+    price: 50,
+    course: "Drinks",
+  },
+];
